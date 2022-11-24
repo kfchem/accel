c02361d0b30ccc7a54c26492022a4cacec81ca40
kfchem <kfukaya@pu-toyama.ac.jp>
2022-11-24 18:48:43 +0900

    icon updated

--- accel/util/dialog.py
+++ accel/util/dialog.py
@@ -13,13 +13,7 @@ class Dialog:
         if not cls.root:
             cls.root = Tk()
             cls.root.withdraw()
-            icon_data = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAACZAAAAmQFwj2B8AAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW\
-                5rc2NhcGUub3Jnm+48GgAAAUBJREFUOI1jYBgFFANGGOM/AwPja0/tbFE2ltsQkb/XGDdeeYyu4X+oJSfDnx9mDH//cn5l+BcKN+Cdt3b577+M2\
-                8V3XLnEwMDA8N9XX5uBiUEG087/3xgEP53+/pYn9cf//w8gQoFGmt999VbgcuZ/BwcOFH6AgcIvP73j/xkYGFn+Oziw/Pr7bhkLy/8sBgYGBoaE\
-                Bg4+HsZMdh5uS1Ye3nN//jOsZqxNv4vs1d///s39859hEhsDw3/G7wEGbUf/cTu6/FXdwMD4z42BgUmdmZX5Ep+C0iUOPj71/6wsjoz//j/4///\
-                /BhYWxnWPT82y+cfwv4SJTU2VcfXqvyySv7QkPzCxMjD8+8vLwMxWycD56ezf1av/vodZ2dDAIsEmZf3/54+Av3+ZV6rrxb6d9XBfssOi1X8JRN\
-                AoIBYAAK0Tda2iIfAiAAAAAElFTkSuQmCC"
-            cls.root.iconphoto(False, PhotoImage(data=icon_data))
+            cls.root.iconphoto(False, PhotoImage(file=Path(__file__).parent.joinpath("logo32.png")))
 
     @classmethod
     def directory(cls, title: str = "Select Directory") -> Path:
