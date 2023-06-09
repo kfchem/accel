b2b959f3edfc184ba2d3428b9a8f603f1932d6ab
kfchem <kfukaya@pu-toyama.ac.jp>
2023-06-09 23:41:07 +0900

    expand_maps implemented and fix calc_stereo

--- accel/base/modeler.py
+++ accel/base/modeler.py
@@ -437,6 +437,7 @@ class Modeler:
         terminal_first: bool = False,
     ) -> list["Atoms"]:
         atoms_map = _get_maps(target, self.atoms, known_pairs=known_pairs, terminal_first=terminal_first)
+        atoms_map = _expand_maps(target, self.atoms, atoms_map)
         atoms_map = _order_maps(target, self.atoms, atoms_map)
         for chain in atoms_map:
             logger.debug(f"maps (target, self): {[(pr[0].number, pr[1].number) for pr in chain]}")
@@ -877,17 +878,20 @@ def _get_maps(
             return False
         return True
 
+    logger.debug(f"generating initial_chains from {len(initial_pairs)} initial_pairs")
     initial_chains: list[list[tuple[Atom]]] = [[]]
-    for initial_pair in initial_pairs:
+    for pair_num, initial_pair in enumerate(initial_pairs, 1):
+        if pair_num % 50 == 0:
+            logger.debug(f"processing initial_pairs: {pair_num}")
         stack = [[initial_pair]]
         while stack:
             side_pairs: list[tuple[Atom]] = stack.pop()
             if len(side_pairs) > len(initial_chains[-1]):
                 initial_chains = [side_pairs]
-                logger.debug(f"initial_chains updated: {[(pr[0].number, pr[1].number) for pr in side_pairs]}")
+                # logger.debug(f"initial_chains updated: {[(pr[0].number, pr[1].number) for pr in side_pairs]}")
             elif len(side_pairs) == len(initial_chains[-1]):
                 initial_chains.append(side_pairs)
-                logger.debug(f"initial_chains appended: {[(pr[0].number, pr[1].number) for pr in side_pairs]}")
+                # logger.debug(f"initial_chains appended: {[(pr[0].number, pr[1].number) for pr in side_pairs]}")
             a_list = [pr[0] for pr in side_pairs]
             b_list = [pr[1] for pr in side_pairs]
             for next_a in side_pairs[-1][0].bonds:
@@ -913,11 +917,11 @@ def _get_maps(
         key = tuple(sorted([(pr[0].number, pr[1].number) for pr in chain], key=lambda t: t[0]))
         canonical_initial_chains[key] = chain
 
-    for chain in canonical_initial_chains.values():
-        logger.debug(f"canonical_initial_chains: {[(pr[0].number, pr[1].number) for pr in chain]}")
+    # for chain in canonical_initial_chains.values():
+    #     logger.debug(f"canonical_initial_chains: {[(pr[0].number, pr[1].number) for pr in chain]}")
 
     min_invalid = None
-    h_matched_chains: list[list[tuple[Atom]]] = [[]]
+    h_matched_canonical_initial_chains: list[list[tuple[Atom]]] = [[]]
     for chain in canonical_initial_chains.values():
         invalid_atoms = 0
         for pair in chain:
@@ -926,13 +930,13 @@ def _get_maps(
             if len(hs_of_a) != len(hs_of_b):
                 invalid_atoms += 1
         if min_invalid is None or min_invalid > invalid_atoms:
-            h_matched_chains = [chain]
+            h_matched_canonical_initial_chains = [chain]
             min_invalid = invalid_atoms
         elif min_invalid == invalid_atoms:
-            h_matched_chains.append(chain)
+            h_matched_canonical_initial_chains.append(chain)
 
-    for chain in h_matched_chains:
-        logger.debug(f"h_matched_chains: {[(pr[0].number, pr[1].number) for pr in chain]}")
+    # for chain in h_matched_canonical_initial_chains:
+    #     logger.debug(f"h_matched_canonical_initial_chains: {[(pr[0].number, pr[1].number) for pr in chain]}")
 
     def _det_ez(a: Atom, b: Atom, c: Atom, d: Atom) -> bool:
         vab = np.array(a.xyz) - np.array(b.xyz)
@@ -978,8 +982,9 @@ def _get_maps(
                 max_dihedral_angle = angle
         return (max_dihedral_a, max_dihedral_angle)
 
+    logger.debug(f"extending {len(h_matched_canonical_initial_chains)} chains")
     extended_chains: list[list[tuple[Atom]]] = []
-    for chain in h_matched_chains:
+    for chain in h_matched_canonical_initial_chains:
         stack = deque([pr for pr in chain])
         assigned_as = given_known_as + [pr[0] for pr in chain]
         assigned_bs = given_known_bs + [pr[1] for pr in chain]
@@ -1171,11 +1176,111 @@ def _get_maps(
     for chain in canonical_recursive_extended_chains.values():
         logger.debug(f"canonical_recursive_extended_chains: {[(pr[0].number, pr[1].number) for pr in chain]}")
 
-    return canonical_recursive_extended_chains.values()
+    return list(canonical_recursive_extended_chains.values())
 
 
-def _correct_maps(atoms_a: "Atoms", atoms_b: "Atoms", atom_maps: list[list[tuple[Atom]]]) -> list[list[tuple[Atom]]]:
-    return atom_maps
+def _expand_maps(
+    atoms_a: "Atoms", atoms_b: "Atoms", atom_maps: list[list[tuple[Atom]]], max_loop: int = 3
+) -> list[list[tuple[Atom]]]:
+    def ab_conv(atom: Atom, atom_map: list[tuple[Atom]]) -> Atom:
+        try:
+            idx = [pr[0] for pr in atom_map].index(atom)
+        except ValueError:
+            pass
+        else:
+            return atom_map[idx][1]
+        try:
+            idx = [pr[1] for pr in atom_map].index(atom)
+        except ValueError:
+            pass
+        else:
+            return atom_map[idx][0]
+        return None
+
+    logger.debug(f"expanding {len(atom_maps)} chains")
+    total_atom_maps_dict: dict[tuple[tuple[int]], list[tuple[Atom]]] = {}
+    for atom_map in atom_maps:
+        key = tuple(sorted([(mp[0].number, mp[1].number) for mp in atom_map], key=lambda t: t[0]))
+        total_atom_maps_dict[key] = atom_map
+    for initial_atom_map in atom_maps:
+        que_atom_maps = [initial_atom_map]
+        loop_counter = 0
+        while que_atom_maps:
+            atom_map = que_atom_maps.pop(0)
+            potential_swaps: list[list[Atom]] = []
+            a_diff_bonds: list[tuple[Atom]] = []
+            for ab_map in atom_map:
+                for a_diff_bd in set([a for a in ab_map[0].bonds]) ^ set(
+                    [ab_conv(b, atom_map) for b in ab_map[1].bonds]
+                ):
+                    a_diff_bonds.append((ab_map[0], a_diff_bd))
+            a_diff_bonds = list(set([tuple(sorted(adb, key=lambda a: a.number)) for adb in a_diff_bonds]))
+            # logger.debug(f"a_diff_bonds: {[[a.number for a in adb] for adb in a_diff_bonds]}")
+            loop_rest_que: list[dict] = [
+                {
+                    "loop_atoms": (adb[0],),
+                    "rest_bonds": [b for b in a_diff_bonds if b is not adb],
+                    "end_atom": adb[1],
+                }
+                for adb in a_diff_bonds
+            ]
+            stored_loop_atoms = []
+            while loop_rest_que:
+                q = loop_rest_que.pop(0)
+                loop_atoms: tuple[Atom] = q["loop_atoms"]
+                rest_bonds: list[tuple[Atom]] = q["rest_bonds"]
+                if loop_atoms[-1] is q["end_atom"]:
+                    stored_loop_atoms.append(loop_atoms)
+                    # logger.debug(f"loop_atoms: {[a.number for a in loop_atoms]}")
+                    if len(set([a.symbol for a in loop_atoms[::2]])) == 1:
+                        potential_swaps.append([a for a in loop_atoms[::2]])
+                    if len(set([a.symbol for a in loop_atoms[1::2]])) == 1:
+                        potential_swaps.append([a for a in loop_atoms[1::2]])
+                for checking_bond in rest_bonds:
+                    if loop_atoms[-1] is checking_bond[0]:
+                        new_terminal = checking_bond[1]
+                    elif loop_atoms[-1] is checking_bond[1]:
+                        new_terminal = checking_bond[0]
+                    else:
+                        continue
+                    loop_rest_que.append(
+                        {
+                            "loop_atoms": loop_atoms + (new_terminal,),
+                            "rest_bonds": [b for b in rest_bonds if b is not checking_bond],
+                            "end_atom": q["end_atom"],
+                        }
+                    )
+            for loop_atoms in stored_loop_atoms:
+                # logger.debug(f"loop_atoms: {[a.number for a in loop_atoms]}")
+                if len(set([a.symbol for a in loop_atoms[::2]])) == 1:
+                    potential_swaps.append([a for a in loop_atoms[::2]])
+                if len(set([a.symbol for a in loop_atoms[1::2]])) == 1:
+                    potential_swaps.append([a for a in loop_atoms[1::2]])
+            potential_swaps: list[tuple[Atom]] = list(
+                set([tuple(sorted(adb, key=lambda a: a.number)) for adb in potential_swaps])
+            )
+            # logger.debug(f"potential_swap: {[[a.number for a in pswap] for pswap in potential_swaps]}")
+            atom_map_a = [m[0] for m in atom_map]
+            for pq_swap in potential_swaps:
+                if len(pq_swap) != 2:
+                    continue
+                try:
+                    p_id: int = atom_map_a.index(pq_swap[0])
+                    q_id: int = atom_map_a.index(pq_swap[1])
+                except ValueError:
+                    logger.error("error on swaping potential_swap")
+                    continue
+                new_map = atom_map[:]
+                new_map[p_id], new_map[q_id] = (pq_swap[0], new_map[q_id][1]), (pq_swap[1], new_map[p_id][1])
+                new_map_key = tuple(sorted([(mp[0].number, mp[1].number) for mp in new_map], key=lambda t: t[0]))
+                if new_map_key not in total_atom_maps_dict:
+                    logger.info(f"expanded_chains: {[(pr[0].number, pr[1].number) for pr in new_map]}")
+                    total_atom_maps_dict[new_map_key] = new_map
+                    que_atom_maps.append(new_map)
+            loop_counter += 1
+            if loop_counter >= max_loop:
+                break
+    return list(total_atom_maps_dict.values())
 
 
 def _order_maps(atoms_a: "Atoms", atoms_b: "Atoms", atom_maps: list[list[tuple[Atom]]]) -> list[list[tuple[Atom]]]:
@@ -1235,6 +1340,7 @@ def _order_maps(atoms_a: "Atoms", atoms_b: "Atoms", atom_maps: list[list[tuple[A
             return True
         return False
 
+    logger.debug(f"ordering {len(atom_maps)} chains")
     evaluated_dicts = [{"map": mp} for mp in atom_maps]
     for evaluated_dic in evaluated_dicts:
         for a in atoms_a:
@@ -1409,17 +1515,28 @@ def _order_maps(atoms_a: "Atoms", atoms_b: "Atoms", atom_maps: list[list[tuple[A
             if det_chirality(*(a_ord_atoms + diff_a_atoms)) == det_chirality(*(b_ord_atoms + diff_b_atoms)):
                 evaluated_dic["invalid_sn2_atoms"] += 1
 
+        evaluated_dic["sp3_carbon_penalty"] = 0
+        for pr in bonding_pairs:
+            if pr[0].symbol != "C" and pr[1].symbol != "C":
+                continue
+            a_singles = pr[0].single
+            b_singles = pr[1].single
+            if len(a_singles) == 4 and len(b_singles) == 4:
+                evaluated_dic["sp3_carbon_penalty"] += 1
+
     evaluated_dicts = sorted(evaluated_dicts, key=lambda d: d["local_rmsd"])
     # evaluated_dicts = sorted(evaluated_dicts, key=lambda d: round(d["bonding_local_rmsd"] * 0.5, 3))
     evaluated_dicts = sorted(evaluated_dicts, key=lambda d: round(d["bonding_tb_local_rmsd"], 1))
     evaluated_dicts = sorted(evaluated_dicts, key=lambda d: d["invalid_sn2_atoms"])
     evaluated_dicts = sorted(evaluated_dicts, key=lambda d: d["sp2_face_transfer_mismatch"])
     evaluated_dicts = sorted(evaluated_dicts, key=lambda d: d["num_of_long_rearranged_atoms"])
+    evaluated_dicts = sorted(evaluated_dicts, key=lambda d: d["sp3_carbon_penalty"])
     return_list = []
     for evaluated_dic in evaluated_dicts:
         mp = [(pr[0].number, pr[1].number) for pr in evaluated_dic["map"]]
         return_list.append(evaluated_dic["map"])
         logger.info(f"mapping: {mp}")
+        logger.info(f"sp3_carbon_penalty: {evaluated_dic['sp3_carbon_penalty']}")
         logger.info(f"num_of_long_rearranged_atoms: {evaluated_dic['num_of_long_rearranged_atoms']}")
         logger.info(f"sp2_face_transfer_mismatch: {evaluated_dic['sp2_face_transfer_mismatch']}")
         logger.info(f"invalid_sn2_atoms: {evaluated_dic['invalid_sn2_atoms']}")
@@ -1607,7 +1724,7 @@ class Chains(MutableSequence):
         return new_chains
 
     def longest(self) -> "Chains":
-        max_len = self.max_distance() + 1
+        max_len = self.max_distance + 1
         return Chains([chain for chain in self._chains if len(chain) == max_len])
 
     def heavily_terminated(self) -> "Chains":
@@ -1699,6 +1816,12 @@ class Chains(MutableSequence):
             sorted_chains_list.extend(next_chains_list)
             return sorted_chains_list
         if len(next_chains_list) == 0 or elongated_flag is False:
+            return None
+        max_dst = max([ch.max_distance for ch in next_chains_list])
+        all_term_atoms: set[Atom] = set()
+        for term_atoms in [ch.longest().terminals for ch in next_chains_list if ch.max_distance == max_dst]:
+            all_term_atoms = all_term_atoms | set(term_atoms)
+        if len(all_term_atoms) == 1:
             return None
         ordered_chains_list = Chains.get_ordered_substituents(next_chains_list)
         if ordered_chains_list is None:
