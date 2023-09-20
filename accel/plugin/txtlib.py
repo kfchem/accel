c94c053159e06859b6b19688cbd8870b63c52841
kfchem <kfukaya@pu-toyama.ac.jp>
2023-09-20 16:04:27 +0900

    data["txt"] type changed to str

--- accel/plugin/txtlib.py
+++ accel/plugin/txtlib.py
@@ -18,8 +18,7 @@ class TxtBox(BoxCore):
     def read_text(self):
         for c in self.get():
             with c.path.open() as f:
-                ls = f.readlines()
-            c.data["txt"] = ls
+                c.data["txt"] = f.read()
         return self
 
     def write_text(self, directory: Path = None, change_path: bool = False, suffix: str = None):
@@ -28,7 +27,7 @@ class TxtBox(BoxCore):
             if suffix is not None:
                 p = p.with_suffix(suffix)
             with p.open("w", newline="\n") as f:
-                f.writelines(c.data["txt"])
+                f.write(str(c.data["txt"]))
             logger.info(f"{p.name} created")
             if change_path:
                 c.path = p
@@ -36,42 +35,53 @@ class TxtBox(BoxCore):
 
     def delete_lines_by_key(self, keyword: str):
         for c in self.get():
-            ls: list[str] = c.data["txt"]
+            ls: list[str] = c.data["txt"].split("\n")
             for i, line in enumerate(ls):
                 if keyword in line:
                     ls.pop(i)
+            c.data["txt"] = "\n".join(ls)
         return self
 
     def delete_lines(self, numbers: list[int]):
         for c in self.get():
-            ls: list[str] = c.data["txt"]
+            ls: list[str] = c.data["txt"].split("\n")
             for num in numbers:
                 ls.pop(int(num - 1))
+            c.data["txt"] = "\n".join(ls)
         return self
 
     def insert_lines(self, number: int, new_lines=""):
         new_lines = _line_formatter(new_lines)
         for c in self.get():
-            ls: list[str] = c.data["txt"]
+            ls: list[str] = c.data["txt"].split("\n")
             ls = ls[: number - 1] + new_lines + ls[number - 2 :]
+            c.data["txt"] = "\n".join(ls)
         return self
 
     def append_lines(self, new_lines=""):
         new_lines = _line_formatter(new_lines)
         for c in self.get():
-            ls: list[str] = c.data["txt"]
+            ls: list[str] = c.data["txt"].split("\n")
             ls.extend(new_lines)
+            c.data["txt"] = "\n".join(ls)
         return self
 
     def replace_lines(self, number: int, new_lines=""):
         new_lines = _line_formatter(new_lines)
         for c in self.get():
-            ls: list[str] = c.data["txt"]
+            ls: list[str] = c.data["txt"].split("\n")
             ls[number - 1] = "".join(new_lines)
+            c.data["txt"] = "\n".join(ls)
+        return self
+
+    def replace_text(self, old_str: str, new_str: str = ""):
+        for c in self.get():
+            c.data["txt"] = c.data["txt"].replace(old_str, new_str)
         return self
 
     def parse_keys(self):
         for c in self.get():
-            ls: list[str] = c.data["txt"]
+            ls: list[str] = c.data["txt"].split("\n")
             ls = replace_key(c, ls)
+            c.data["txt"] = "\n".join(ls)
         return self
